e7a2b632fa031da2499f7e07ae118d68d129209c
Nouman Javed <noumanmalik960@gmail.com>
2021-01-01 23:12:17 +0500

    storyShot addon added for testing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 
+// .storybook/preview.js
+
+import '../src/index.css'
+
+// Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
